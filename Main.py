ee53386ee2ae2663ce428c134073e8f805bcedd9
Clifton McCook <cliftonmccook@gmail.com>
2020-11-28 17:18:45 -0500

    edited main

--- Main.py
+++ Main.py
@@ -5,17 +5,6 @@ from UI import interpreterui
 
 if __name__ == '__main__':
     m = manager.InterpretationManager()
-
-    '''m.addLanguage("SPANISH")
-    m.addLanguage("FRENCH")
-    m.addInterpreter("John","ENGLISH","SPANISH","john@tia.com")
-    m.addInterpreter("Mary","ENGLISH","FRENCH","mary@tia.com")
-
-    john = m.interpretersList[0]
-    mary = m.interpretersList[1]
-
-    john.printinfo()
-    mary.printinfo()'''
     s = setup.setupUI(m).go()
 
 
