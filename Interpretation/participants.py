09744b8d3c82f98570047bc8e7fe89fe455e1579
moniquehg <75190980+moniquehg@users.noreply.github.com>
2020-11-30 16:51:26 -0500

    Update participants.py

--- Interpretation/participants.py
+++ Interpretation/participants.py
@@ -1,8 +1,10 @@
 class MeetingParticipant:
+    """"Constructor: set participant's name and source language"""
     def __init__(self, name):
         self.name = name
         self.sourcelang = "ENGLISH"
-
+        
+    """Method allows for participant to select their source language"""
     def selectSourceLanguage(self, manager, lang):
         templang = ""
         for i, j in enumerate(manager.availableLanguages):
@@ -13,18 +15,23 @@ class MeetingParticipant:
             print(f'You are now listening to {lang}')
         else:
             print(f'{lang} is not available as a source language')
-
+    
+    """Getter: Prints the name of the specified participant"""
     def printinfo(self):
         print(self.name)
 
 
 class Interpreter(MeetingParticipant):
+    
+    """Constructor: sets the name, source language,
+        target language and email of the interpreter""""
         def __init__(self, name, sourcelang, targetlang, email):
             super().__init__(name)
             self.sourcelang = sourcelang
             self.targetlang = targetlang
             self.email = email
 
+     """"Method to set the interpreter's target language"""
         def selectTargetLanguage(self, manager, lang):
             templang = ""
             for i, j in enumerate(manager.availableLanguages):
@@ -35,10 +42,12 @@ class Interpreter(MeetingParticipant):
                 print(f'You are now interpreting to {lang}')
             else:
                 print(f'{lang} is not available as a target language')
-
+                
+    """Method to send a handover request to a specified user"""
         def requestHandover(self, interpreter):
             print(f'{self.name} requesting handover to {interpreter.name}...')
-
+            
+    """Method to accept a handover request from a user"""
         def acceptHandover(self,manager, interpreter):
             self.selectTargetLanguage(manager, interpreter.targetlang)
             self.selectSourceLanguage(manager, interpreter.sourcelang)
@@ -47,7 +56,7 @@ class Interpreter(MeetingParticipant):
 
 
 class Attendee(MeetingParticipant):
-    """TODO"""
+    """Constructor: sets the name of the attendees of the meeting"""
     def __init__(self, name):
         super().__init__(name)
     pass
