856070ee95099c14a6ecb361b519cd0ea18fc353
maraao <75191475+maraao@users.noreply.github.com>
2020-12-03 02:45:40 -0500

    Update interpreterui.py

--- UI/interpreterui.py
+++ UI/interpreterui.py
@@ -47,8 +47,6 @@ class InterpreterUI:
                         q.acceptHandover(self.manager, name)
                         self.dashboard(q)
                         print ("Handover request sent")
-                    else:
-                        print ("The person entered is not an available interpreter")
                 break
             if option == '7':
                 print("\nEXITING INTERPRETER DASHBOARD...")
