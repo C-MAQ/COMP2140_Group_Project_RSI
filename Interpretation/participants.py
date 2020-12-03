45253c7028685fd0ffa399bcd815f5bcb9cee043
maraao <75191475+maraao@users.noreply.github.com>
2020-12-03 03:58:10 -0500

    Update participants.py

--- Interpretation/participants.py
+++ Interpretation/participants.py
@@ -50,7 +50,7 @@ class Interpreter(MeetingParticipant):
             self.selectTargetLanguage(manager, interpreter.targetlang)
             self.selectSourceLanguage(manager, interpreter.sourcelang)
             print(f'{self.name} accepted handover')
-            print(f'{self.name} is now interpreting into {self.targetlang}')
+            print(f'{self.name} is now interpreting to {self.targetlang}')
 
 
 class Attendee(MeetingParticipant):
